e281c41782f3d6f72cc5c4995b39cb1dceeb9482
wqs <15775831018@163.com>
2016-12-29 13:24:33 +0800

    Init my first app

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <header></header>
+    <img src="./assets/logo.png">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import Header from './header/Header'
+
+export default {
+  name: 'app',
+  components: {
+    Hello,
+    Header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/header/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="header">
+        <div>this is header</div>
+    </div>
+</template>
+<style>
+    .header{
+    width:100%;
+    height: 30px;
+    }
+</style>
+<script>
+export default {
+  name: 'header',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
